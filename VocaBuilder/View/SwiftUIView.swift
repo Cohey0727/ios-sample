4a2ca84f673984771558fd24497dde884cb60d39
KoheiOkamoto <ohayousagi.ac.kook0727@gmail.com>
2024-06-14 19:45:49 +1000

    main

---
+++ VocaBuilder/View/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  VocaBuilder
+//
+//  Created by 岡本耕平 on 2024/06/14.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
